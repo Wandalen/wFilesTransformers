2f6d624b0a760cca5b72717811bca9262d6d1112
Wandalen <wandalen.me@gmail.com>
2018-11-05 17:09:29 +0200

    auto

--- proto/dwtools/amid/files/l9/FilesTransformers.s
+++ proto/dwtools/amid/files/l9/FilesTransformers.s
@@ -184,7 +184,7 @@ let Self = wTools.FileTransformers = ReadEncoders;
 
 if( typeof module !== 'undefined' )
 if( _global_.WTOOLS_PRIVATE )
-delete require.cache[ module.id ];
+{ /* delete require.cache[ module.id ]; */ }
 
 if( typeof module !== 'undefined' && module !== null )
 module[ 'exports' ] = Self;
